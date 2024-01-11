2343e5e86b039a2cb21ccc7c7f940115977d4b08
Aynazar <aynazar.abdullin102@gmail.com>
2024-01-11 17:35:27 +0500

    add product folder

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { CurrentUser } from '@common/decorators';
-import { User } from '@prisma/client';
+import { CurrentUser, Roles } from '@common/decorators';
+import { Role, User } from '@prisma/client';
 import { CreateProductDto } from './dto/create-product.dto';
+import { RolesGuard } from '../auth/guards/Role.guard';
 
 @Controller('product')
+@UseGuards(RolesGuard)
+@Roles(Role.ADMIN)
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
